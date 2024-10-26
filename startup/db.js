aaa66ba75229c578501ca9df35cf3dd43d454f9d
Arash Afsharian <arash.afsharian1996@gmail.com>
2024-10-26 11:48:55 +0200

    Add mlab database in cloud

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/PopcornFlicks')
+    mongoose.connect(process.env.PopcornFlicks_db)
         .then(() => console.log('Connected to MongoDB...'))
         .catch((err) => console.error('Could not connect to MongoDB!', err))
 }
