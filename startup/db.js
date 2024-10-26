0f453656acaf71ee9f175163dc2eae3466fd1f21
Arash Afsharian <arash.afsharian1996@gmail.com>
2024-10-26 12:11:48 +0200

    Finalizing

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = function () {
-    mongoose.connect('mongodb+srv://arashafsharian1996:PWY756LMAomvxpyP@popcornflicks.nmd0j.mongodb.net/?retryWrites=true&w=majority&appName=PopcornFlicks')
+    mongoose.connect(process.env.PopcornFlicks_db)
         .then(() => console.log('Connected to MongoDB...'))
         .catch((err) => console.error('Could not connect to MongoDB!', err))
 }
